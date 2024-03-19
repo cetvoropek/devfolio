a96e36dfd310327d541bd6ad82e61cc9da6a98da
fsociety <72711660+cetvoropek@users.noreply.github.com>
2024-03-19 18:03:32 +0100

    INITIAL COMMIT

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import bg from "../../public/background/home-background.png"
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between relative">
+      <Image src={bg} alt="background-image" className="w-full h-full object-cover object-center opacity-25"/>
+    </main>
+  );
+}
